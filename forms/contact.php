d442899fd20e7de5ac868be1d26996ef41dccff9
franlecam <69166136+franlecam@users.noreply.github.com>
2025-11-20 14:48:30 -0300

    Upload

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,41 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+// Dirección donde recibirás los mensajes
+$receiving_email_address = "vector.regional.consultores@gmail.com";
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+// Verifica que se hayan enviado los datos
+if (
+    !isset($_POST['name']) ||
+    !isset($_POST['email']) ||
+    !isset($_POST['subject']) ||
+    !isset($_POST['message'])
+) {
+    die("Error: Missing fields");
+}
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+// Captura los datos
+$name = strip_tags($_POST['name']);
+$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+$subject = strip_tags($_POST['subject']);
+$message = strip_tags($_POST['message']);
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+$headers = "From: $name <$email>\r\n";
+$headers .= "Reply-To: $email\r\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+// Envía el mail
+$sent = mail(
+    $receiving_email_address,
+    $subject,
+    "Nombre: $name\nEmail: $email\n\nMensaje:\n$message",
+    $headers
+);
 
-  echo $contact->send();
+// Respuesta al AJAX
+if ($sent) {
+    echo "OK";
+} else {
+    echo "Error: mensaje no enviado.";
+}
+
 ?>
